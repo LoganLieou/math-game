8bbca19f17f939d0bc1db7eb57dc0f39ad674f2a
LoganLieou <logan.lieou@gmail.com>
2023-09-16 16:34:36 -0500

    Initial Commit

---
+++ math-game/math_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  math_gameApp.swift
+//  math-game
+//
+//  Created by Logan  Jackson on 9/16/23.
+//
+
+import SwiftUI
+
+@main
+struct math_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
